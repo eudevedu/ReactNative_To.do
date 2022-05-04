ced9ed833de30a2a702ac2bb226201186616045a
Eduardo Batista <72777045+EduardoBatista1@users.noreply.github.com>
2022-05-04 11:43:31 -0500

    updateApp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 }
 
 export function Header({ tasksCounter }: HeaderProps) {
-  // const tasksCounterText = TODO render 'tarefa' if tasksCounter equals 1, otherwise render 'tarefas'
+   const tasksCounterText  = tasksCounter === 1? 'tarefa' : 'tarefas';
   
   return (
     <View style={styles.container}>
@@ -17,7 +17,7 @@ export function Header({ tasksCounter }: HeaderProps) {
       
       <View style={styles.tasks}>
         <Text style={styles.tasksCounter}>Você tem </Text>
-        {/* <Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text> */}
+        <Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text>
       </View>
     </View>
   )
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
     paddingTop: getStatusBarHeight(true) + 16,
     paddingHorizontal: 24,
     paddingBottom: 60,
-    backgroundColor: '#8257E5',
+    backgroundColor: '#ee4d2d',
     justifyContent: 'space-between',
     alignItems: 'center',
     flexDirection: 'row'
